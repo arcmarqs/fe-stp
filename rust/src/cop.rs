bd9bee5b2ac163e3135e3e0fd7a218144b2e5782
arcmarqs <maramadeu@gmail.com>
2023-09-09 11:01:12 +0100

    increase range

--- rust/src/cop.rs
+++ rust/src/cop.rs
@@ -377,7 +377,7 @@ fn run_client(client: SMRClient) {
 
     for u in 0..500000 {
 
-        let i = rand.gen_range(1..2000);
+        let i = rand.gen_range(1..10000);
 
         let kv = format!("{}{}", id.0.to_string(), u.to_string());
 
