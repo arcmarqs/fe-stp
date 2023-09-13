a4f06102da23e3068f19e473da31c89209e76dbf
arcmarqs <maramadeu@gmail.com>
2023-09-13 16:28:00 +0100

    change client

--- rust/src/cop.rs
+++ rust/src/cop.rs
@@ -382,7 +382,7 @@ fn run_client(client: SMRClient) {
 
         let kv = format!("{}{}", id.0.to_string(), u.to_string());
     
-        let request = if u % 3 == 0 {
+        let request = if u > 150000 {
             Action::Read(i.to_be_bytes().to_vec())
         } 
         else {
