ac92e4059224c8b85eea3d37db5e385e303e9f6b
arcmarqs <maramadeu@gmail.com>
2023-09-28 15:41:37 +0100

    change executor

--- rust/src/exec.rs
+++ rust/src/exec.rs
@@ -1,85 +1,87 @@
-use std::ops::Deref;
-use std::sync::{Arc, Weak};
+use std::sync::Arc;
 
-use atlas_common::async_runtime::spawn;
-use atlas_common::collections::HashMap;
-use atlas_execution::state::divisible_state::DivisibleState;
-use chrono::DateTime;
-use chrono::offset::Utc;
+use atlas_divisible_state::state_orchestrator::StateOrchestrator;
+
 use atlas_common::error::*;
-use atlas_common::ordering::SeqNo;
-use atlas_common::node_id::NodeId;
-use atlas_execution::app::{Application, BatchReplies, Reply, Request, UpdateBatch};
-use atlas_metrics::benchmarks::{BenchmarkHelperStore, Measurements};
 
-use crate::serialize::{self, State};
+use atlas_execution::app::{Application, BatchReplies, Reply, Request, UpdateBatch};
+
+use crate::serialize;
 
 use crate::serialize::KvData;
 
 #[derive(Default)]
 pub struct KVApp;
 
-impl Application<State> for KVApp {
+impl Application<StateOrchestrator> for KVApp {
     type AppData = KvData;
 
-fn initial_state() -> Result<State> {
- 
-    let state = State::new();   
+fn initial_state() -> Result<StateOrchestrator> {
+    // create the state
+
+    let id: u32 = std::env::args()
+    .nth(1).expect("No replica specified")
+    .trim().parse().expect("Expected an integer");
+
+    let path = format!("{}{}", "./appdata_",id);
+
+    let state = StateOrchestrator::new(&path);   
             
     Ok(state)
 }
 
-fn unordered_execution(&self, state: &State, request: Request<Self, State>) -> Reply<Self, State> {
+fn unordered_execution(&self, state: &StateOrchestrator, request: Request<Self, StateOrchestrator>) -> Reply<Self, StateOrchestrator> {
         todo!()
     }
 
     fn update(
         &mut self,
-        state: &mut State,
-        request: Request<Self, State>,
-    ) -> Reply<Self, State> {
+        state: &mut StateOrchestrator,
+        request: Request<Self, StateOrchestrator>,
+    ) -> Reply<Self, StateOrchestrator> {
         let reply_inner = match request.as_ref() {
             serialize::Action::Read(key) => {
-                let ret = state.db.get(key);
-
-                match ret {
+                match state.get(key) {
                     Some(vec) => {
-                        let map: HashMap<String, Vec<u8>> = bincode::deserialize(vec.as_ref()).expect("deserialize");
 
-                        serialize::Reply::Single(map)
+                        serialize::Reply::Single(vec.as_ref().to_owned())
                     },
                     None => serialize::Reply::None,
                 }
 
             }
             serialize::Action::Insert(key, value) => {
-                let serialized_map = bincode::serialize(value).expect("failed to serialize");
-                state.db.insert(key.clone(), serialized_map);
+                
+                match state.insert(key, value.to_owned()) {
+                    Some(vec) => {
 
-                serialize::Reply::None
+                        serialize::Reply::Single(vec.as_ref().to_owned())
+                    },
+                    None => serialize::Reply::None,
+                }
             }
             serialize::Action::Remove(key) => { 
-                let ret = state.db.remove(key);
-                
-                match ret {
-                    Some(vec) => {
-                        let map: HashMap<String, Vec<u8>> = bincode::deserialize(vec.as_ref()).expect("deserialize");
 
-                        serialize::Reply::Single(map)
+                match state.remove(key) {
+                    Some(vec) => {
+
+                        serialize::Reply::Single(vec.as_ref().to_owned())
                     },
                     None => serialize::Reply::None,
                 }
             }
         };
 
+       // state.db.flush();
+
         Arc::new(reply_inner)
     }
 
 fn unordered_batched_execution(
         &self,
-        state: &State,
-        requests: atlas_execution::app::UnorderedBatch<Request<Self, State>>,
-    ) -> BatchReplies<Reply<Self, State>> {
+        state: &StateOrchestrator,
+        requests: atlas_execution::app::UnorderedBatch<Request<Self, StateOrchestrator>>,
+    ) -> BatchReplies<Reply<Self, StateOrchestrator>> {
         let mut reply_batch = BatchReplies::with_capacity(requests.len());
 
         for unordered_req in requests.into_inner() {
@@ -93,11 +95,11 @@ fn unordered_batched_execution(
 
 fn update_batch(
         &mut self,
-        state: &mut State,
-        batch: UpdateBatch<Request<Self, State>>,
-    ) -> BatchReplies<Reply<Self, State>> {
+        state: &mut StateOrchestrator,
+        batch: UpdateBatch<Request<Self, StateOrchestrator>>,
+    ) -> BatchReplies<Reply<Self, StateOrchestrator>> {
         let mut reply_batch = BatchReplies::with_capacity(batch.len());
-
+    
         for update in batch.into_inner() {
             let (peer_id, sess, opid, req) = update.into_inner();
             let reply = self.update(state, req);
